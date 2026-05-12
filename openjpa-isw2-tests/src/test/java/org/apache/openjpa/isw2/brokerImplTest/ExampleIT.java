9cb8164ff63ef54bc059faec461f6b4662c90fa9
kuro1999 <edoardo.marchionni99@gmail.com>
2026-05-12 17:17:18 +0200

    cambiati i pom così c e nel parent surefire e failsafe per test di unita e di integrazione.+ prova github actions

---
+++ openjpa-isw2-tests/src/test/java/org/apache/openjpa/isw2/brokerImplTest/ExampleIT.java
@@ -0,0 +1,13 @@
+package org.apache.openjpa.isw2.brokerImplTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+
+    @Test
+    public void shouldPassSimpleIntegrationTest() {
+        assertTrue(true);
+    }
+}
---
+++ openjpa-isw2-tests/src/test/java/org/apache/openjpa/isw2/joinTest/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.apache.openjpa.isw2.joinTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void shouldPassSimpleUnitTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
